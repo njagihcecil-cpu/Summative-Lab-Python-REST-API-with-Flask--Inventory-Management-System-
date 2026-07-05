17a3f1e70b03309bb300e0a2a626af950fdff512
njagih <njagih600@gmail.com>
2026-07-05 22:06:27 +0300

    add mock database

---
+++ inventory.py
@@ -0,0 +1,18 @@
+inventory = [
+    {
+        "id": 1,
+        "barcode": "3017620422003",
+        "name": "Nutella",
+        "brand": "Ferrero",
+        "price": 450,
+        "stock": 20
+    },
+    {
+        "id": 2,
+        "barcode": "5449000000996",
+        "name": "Coca-Cola",
+        "brand": "The Coca-Cola Company",
+        "price": 120,
+        "stock": 35
+    }
+]
